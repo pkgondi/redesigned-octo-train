3bf0b7eed0ee34db647b289b991fa4266bc33a73
pkgondi <phaneendra.gondi@sainsburys.co.uk>
2022-03-31 12:31:46 +0100

    “seconfcommit”

--- src/test/kotlin/com/githubactions/regression/ApplicationTests.kt
+++ src/test/kotlin/com/githubactions/regression/ApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.githubactions.regression
 
+import jdk.jfr.Description
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest
 class ApplicationTests {
 	private val testSample: Sample = Sample()
 	@Test
+	@Description("Test")
 	fun contextLoads() {
 		val expected = 42
 		assertEquals(expected, testSample.sum(40, 2))
