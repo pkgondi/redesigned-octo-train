f643321ad624417ec8a413aad1859875646c1c26
pkgondi <phaneendra.gondi@sainsburys.co.uk>
2022-03-31 12:21:03 +0100

    first_commit

---
+++ src/test/kotlin/com/githubactions/regression/ApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.githubactions.regression
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+	private val testSample: Sample = Sample()
+	@Test
+	fun contextLoads() {
+		val expected = 42
+		assertEquals(expected, testSample.sum(40, 2))
+	}
+
+}
